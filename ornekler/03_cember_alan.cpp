73b79c292adf2daaf16f78bc3b37db46434c3b8d
Ali Can Canbay <lcnbyy@gmail.com>
2019-04-21 01:21:41 +0300

    Update 03_cember_alan.cpp

--- ornekler/03_cember_alan.cpp
+++ ornekler/03_cember_alan.cpp
@@ -1,3 +1,7 @@
+/*
+1. Kullanicidan yaricap bilgisi alinir
+2. Cemberin cevresi ve yaricapi ekrana bastirilir
+*/
 #include<iostream>
 #define _USE_MATH_DEFINES // Matematik sabitleri
 #include<cmath> // Matematik fonksiyonlari
@@ -5,9 +9,11 @@ using namespace std;
 
 int main(){
 	int r;
+	//1
 	cout << "Cemberin yaricapini giriniz..." <<endl;
 	cin >> r;
-	cout << "Cemberin Cevresi: " << 2*M_PI*r << endl;
+	//2
+	cout << "Cemberin Cevresi: " << 2*M_PI*r << endl;  // M_PI, cmath kutuphanesinden gelen pi sayisini veren tanimdir
 	cout << "Cemberin Alani: " << M_PI*pow(r,2) << endl;
 	return 0;
 }
