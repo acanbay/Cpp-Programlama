cd76c4c08c2955d13e1811958510bc3ef09ad0bb
Ali Can Canbay <lcnbyy@gmail.com>
2019-04-21 01:24:47 +0300

    Update 05_harf_notu.cpp

--- ornekler/05_harf_notu.cpp
+++ ornekler/05_harf_notu.cpp
@@ -4,15 +4,19 @@
   50-70  	C
   70-90  	B
   90-100	C
+  
+1. Kullanicidan not bilgisi alinir
+2. Notuna karsilik gelen harf degeri bastirilir
 */
 #include<iostream>
 using namespace std;
 
 int main(){
 	int not_;
+	//1
 	cout << "Notu giriniz..." << endl;
 	cin >> not_ ;
-	
+	//2
 	if(not_>=90){
 		cout << "Harf notu: A" << endl;
 	}
