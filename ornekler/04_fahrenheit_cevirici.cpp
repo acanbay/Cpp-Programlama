cb56a6bb932a8d06c6b8221e2fe29965b0a03624
Ali Can Canbay <lcnbyy@gmail.com>
2019-04-21 01:23:24 +0300

    Update 04_fahrenheit_cevirici.cpp

--- ornekler/04_fahrenheit_cevirici.cpp
+++ ornekler/04_fahrenheit_cevirici.cpp
@@ -1,9 +1,17 @@
+/*
+F=(C*1.8)+32
+
+1. Kullaniciden santigrat deger alinir
+2. Alinan deger fahrenheit'e cevirilerek ekrana bastirilir
+*/
 #include<iostream>
 using namespace std;
 int main(){
-double ctemp;
-cout<<"sicaklik(C) girin:"<<endl;
-cin>>ctemp;
-cout<<"sicaklik(F): "<< (ctemp*1.8)+32<<endl;
-return 0; 
+  double ctemp;
+  //1
+  cout<<"sicaklik(C) girin:"<<endl;
+  cin>>ctemp;
+  //2
+  cout<<"sicaklik(F): "<< (ctemp*1.8)+32<<endl;
+  return 0; 
 }
