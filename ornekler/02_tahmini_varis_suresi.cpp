7aafc486e9870b3f51e36ac12e1c711066c7995e
Ali Can Canbay <lcnbyy@gmail.com>
2019-04-21 01:20:14 +0300

    Update 02_tahmini_varis_suresi.cpp

--- ornekler/02_tahmini_varis_suresi.cpp
+++ ornekler/02_tahmini_varis_suresi.cpp
@@ -1,16 +1,24 @@
+/* Kullanicidan gidilecek mesafe ve aracin hizi alinarak tahmini varis suresi hesaplanacak
+1. Kullanicidan bilgiler alinir
+2. Saat dakika donusumleri yapilir
+3. Tahmini varis suresi kullaniciya bastirilir
+*/
 #include<iostream>
 #include<math.h>
 using namespace std;
 
 int main(){
 	int yol,hiz;
+	//1
 	cout<<"Mesafeyi giriniz"<<endl;
 	cin>>yol;
 	cout<<"Hizi giriniz"<<endl;
 	cin>>hiz;
-	cout<<"Tahmini varis sureniz: "<<endl;
+	//2
 	int saat =yol/hiz;
-	int dakika = ((float)yol/hiz-(int)yol/hiz)*60;
+	int dakika = ((float)yol/hiz-(int)yol/hiz)*60; // Virgulden sonraki bolumler dakikaya cevirilir
+	//3
+	cout<<"Tahmini varis sureniz: "<<endl;
 	cout<<saat<<" saat ve "<<dakika<<" dakikadir."<<endl;	
 	return 0;
 }
